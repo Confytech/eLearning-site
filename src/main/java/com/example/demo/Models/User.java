aafe57ca7ba228803448e869b754d2217680d309
confytech <confiehiemere@gmail.com>
2024-07-27 22:43:30 +0100

    Fixed case-sensitive filename issues

--- src/main/java/com/example/demo/Models/User.java
+++ src/main/java/com/example/demo/Models/User.java
@@ -1,22 +1,43 @@
 package com.example.demo.Models;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "app_user")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String password;
 
-    public void setPassword(String encode) {
+    public Long getId() {
+        return id;
     }
 
-    public CharSequence getPassword() {
-        return null;
+    public void setId(Long id) {
+        this.id = id;
     }
-    // Getters and Setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
